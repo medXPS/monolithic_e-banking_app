3211867c1ab7fc7472b2584a27ccf661f9647d94
medXPS <mohamedammaha2020@gmail.com>
2023-08-18 17:18:01 +0100

    adding resources

--- src/main/java/com/jnkTest/demo/controller/DemoController.java
+++ src/main/java/com/jnkTest/demo/controller/DemoController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/test")
     public String test(){
-        return "we will use this endpoint for deploying an automated email sender ";
+        return "hello it works! ";
     }
 }
